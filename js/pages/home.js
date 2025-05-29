4d33c601bbb995eae6c4be15b4bfdb12f591f120
Ernest <zyperis14@gmail.com>
2025-05-29 17:22:30 +0300

    main-row2

--- js/pages/home.js
+++ js/pages/home.js
@@ -5,11 +5,13 @@ import { header } from "../components/header.js";
 import { mainHeader } from "../components/main-header.js";
 import { mainMainrow } from "../components/mainMainrow.js";
 import { herofooter } from "../components/hero-footer.js";
+import { mainRow2 } from "../components/main-row2.js";
 
 
 header();
 mainHeader();
 mainMainrow();
 herofooter();
+mainRow2();
 
 
