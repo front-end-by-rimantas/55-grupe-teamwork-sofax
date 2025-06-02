7dae4cf1bacb964d7809eb5e293c0fb01deaf336
Ernest <zyperis14@gmail.com>
2025-06-02 10:42:57 +0300

    footer update

--- js/components/footer.js
+++ js/components/footer.js
@@ -1,30 +1,30 @@
 export function footer() {
   const HTML = `
-        <footer class="container">
+            <footer class="container">
       <div class="row">
         <div class="footer-top">
-          <div class="footer-about col-12 col-xl-8">
+          <div class="footer-about">
             <h1>Let's embark on a IT services together!</h1>
             <img src="../img/shape1.png" alt="" />
           </div>
-          <div class="arrow-up col-xl-1 m-xl-3">
+          <div class="arrow-up">
             <a href="#">
               <i class="fa fa-arrow-right"></i>
             </a>
           </div>
         </div>
         <div class="footer-bottom">
-          <div class="socials col-lg-6 col-xl-4">
+          <div class="socials col-md-12 col-xl-4">
             <img src="../img/SofaxLogo.webp" alt="" />
             <p>Sofax has many plans for the future to work with great clients and continue to work with agencies.</p>
             <div class="social-links">
-              <a href="#" class="fa fa-xing"></a>
-              <a href="#" class="fa fa-facebook"></a>
-              <a href="#" class="fa fa-instagram"></a>
-              <a href="#" class="fa fa-linkedin"></a>
+              <a href="#" target="_blank" class="fa fa-twitter"></a>
+              <a href="#" target="_blank" class="fa fa-facebook"></a>
+              <a href="#" target="_blank" class="fa fa-instagram"></a>
+              <a href="#" target="_blank" class="fa fa-linkedin"></a>
             </div>
           </div>
-          <div class="company col-lg-4 col-xl-3">
+          <div class="company col-12 col-md-3 m-md-0 m-lg-1 col-xl-3 m-xl-0">
             <p>Company</p>
             <ul>
               <li><a href="#">About Us</a></li>
@@ -33,7 +33,7 @@ export function footer() {
               <li><a href="#">Terms &amp; Conditions</a></li>
             </ul>
           </div>
-          <div class="utility col-lg-4 col-xl-2">
+          <div class="utility col-12 col-md-3 col-xl-2">
             <p>Utility pages</p>
             <ul>
               <li><a href="#">Instructions</a></li>
@@ -42,7 +42,7 @@ export function footer() {
               <li><a href="#">Licenses</a></li>
             </ul>
           </div>
-          <div class="subscribe col-lg-4 col-xl-3">
+          <div class="subscribe col-md-4 col-xl-3">
             <p>Subscribe to our newsletter</p>
             <form class="form">
               <input class="white" type="email" placeholder="Enter your email" name="email" id="email" maxlength="50" required />
@@ -50,7 +50,7 @@ export function footer() {
             </form>
           </div>
         </div>
-        <div class="copy-right">
+        <div class="copy-right col-12">
           <p>© 2025 Mthemeus All Rights Reserved.</p>
         </div>
       </div>
