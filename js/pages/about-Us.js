e087805b237f524cfe46f849f111f79678c0304b
Povilas Dailyda <povilas.dailyda@gmail.com>
2025-06-02 15:24:32 +0300

    pradzia reiks keisti spalvas keisti vidini html

--- js/pages/about-Us.js
+++ js/pages/about-Us.js
@@ -1,2 +1,18 @@
 import { header } from "../components/header.js";
 header();
+
+let HTML = `<div class="container seniukas">
+<div class="row">
+
+
+
+</div>
+
+</div>`
+
+
+
+document
+.getElementById('app')
+.insertAdjacentHTML('afterbegin', HTML);
+
