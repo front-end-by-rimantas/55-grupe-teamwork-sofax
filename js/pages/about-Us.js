4b5d962e3aeee8f5c2cb0aecd1e9ea59f3c70e2b
Ernest <zyperis14@gmail.com>
2025-06-03 16:26:01 +0300

    about us main row 2

--- js/pages/about-Us.js
+++ js/pages/about-Us.js
@@ -1,5 +1,6 @@
 import { aboutUsHeader } from "../components/about-us/about-us-header.js";
+import { aboutUsMainRow2 } from "../components/about-us/about-us-main-row2.js";
 
 aboutUsHeader();
-
+aboutUsMainRow2();
 
