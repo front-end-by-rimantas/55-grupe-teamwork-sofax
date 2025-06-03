8f74d23b5ae220613edd77ab4e35248af1884042
Povilas Dailyda <povilas.dailyda@gmail.com>
2025-06-03 09:21:57 +0300

    correction

--- js/components/about-us/about-us-header.js
+++ js/components/about-us/about-us-header.js
@@ -5,7 +5,7 @@ export function aboutUsHeader () {
         <div class="row">
             <header class="col-12 headeris">
             <div>
-            <a href = "#"><img class="logo" src="../../../img/SofaxLogo.webp" alt="logo" ></a>
+            <a href = "#"><img class="logo" src="../../../img/logo-white-true.webp" alt="logo-white" ></a>
             </div>
         <nav class="navbar">
             <a href="#">Demos</a>
@@ -19,15 +19,16 @@ export function aboutUsHeader () {
             </header>
             
         </div>
-    </div>
-    <div class="col-12 forh1">
+          <div class="col-12 forh1">
             <h1>About Us</h1>
             <div class="forHome">
             <a href="#">Home</a>
             <i class="fa fa-chevron-right" aria-hidden="true"></i>
             <a href="#" >About Us</a>
             </div>
-            </div>`;
+            </div>
+    </div>
+  `;
  
     document
         .getElementById('app')
