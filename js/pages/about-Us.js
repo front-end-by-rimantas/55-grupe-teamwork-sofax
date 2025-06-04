fa7ac530e185b7813138a5446b32062ee8d7e2d7
Povilas Dailyda <povilas.dailyda@gmail.com>
2025-06-04 13:58:00 +0300

    AboutUsrow3 su responsive

--- js/pages/about-Us.js
+++ js/pages/about-Us.js
@@ -1,9 +1,12 @@
 import { aboutUsHeader } from "../components/about-us/about-us-header.js";
 import { aboutUsMain } from "../components/about-us/about-us-Main.js";
 import { aboutUsMainRow2 } from "../components/about-us/about-us-main-row2.js";
+import { aboutUsMainRow3 } from "../components/about-us/about-us-MainRow3.js";
 
 aboutUsHeader();
 aboutUsMain();
 aboutUsMainRow2();
+aboutUsMainRow3();
+
 
 
