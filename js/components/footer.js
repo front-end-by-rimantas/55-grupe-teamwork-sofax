7627833d952fa2aa2ea9fec805b8a0f98720cc61
Povilas Dailyda <povilas.dailyda@gmail.com>
2025-06-04 10:43:52 +0300

    pataisymas su taskais

--- js/components/footer.js
+++ js/components/footer.js
@@ -5,7 +5,7 @@ export function footer() {
         <div class="footer-top">
           <div class="footer-about">
             <h1>Let's embark on a IT services together!</h1>
-            <img src="../img/shape1.png" alt="" />
+            <img src="./img/shape1.png" alt="logo" />
           </div>
           <div class="arrow-up">
             <a href="#">&#8594;</a>
@@ -13,11 +13,11 @@ export function footer() {
         </div>
         <div class="footer-bottom">
           <div class="socials col-md-12 col-xl-4">
-            <img src="../img/SofaxLogo.webp" alt="" />
+            <img src="./img/SofaxLogo.webp" alt="logo" />
             <p>Sofax has many plans for the future to work with great clients and continue to work with agencies.</p>
             <div class="social-links">
               <a href="#" target="_blank">
-               <img src="../img/x.png" alt="X">
+               <img src="./img/x.png" alt="X">
               </a>
               <a href="#" target="_blank" class="fa fa-facebook"></a>
               <a href="#" target="_blank" class="fa fa-instagram"></a>
