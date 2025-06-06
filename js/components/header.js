5ea0034ae97c831d6c49e2ffdc8f5a6ef0710b4f
Ernest <zyperis14@gmail.com>
2025-06-06 13:21:12 +0300

    sub meniu stilius

--- js/components/header.js
+++ js/components/header.js
@@ -5,23 +5,39 @@ export function header() {
     let navbarHTML = ``;
 
     for (const item of headerData) {
-        navbarHTML += `<a href="${item.href}">${item.text}</a>`;
+        if (item.subMenu) {
+
+            let subMenuHTML = '';
+
+            for (const subItem of item.subMenu) {
+                subMenuHTML += `
+                <a href="${subItem.href}">${subItem.text}</a>
+            `
+            };
+
+            navbarHTML += `
+            <div class="dropdown">
+                <a href="${item.href}">${item.text}<i class="fa fa-angle-down"></i></a>
+                <div class="dropdown-list"><i class="fa fa-caret-up top-angle"></i>${subMenuHTML}</div>
+            </div>`;
+        } else {
+            navbarHTML += `<div class="dropdown"><a href="${item.href}">${item.text}</a></div>`;
+        }
     }
 
     const HTML = `
     <div class="container-header">
         <div class="row">
             <header class="col-12 headeris">
-            <div>
-            <a href = "#"><img class="logo" src="./img/SofaxLogo.webp" alt="logo" ></a>
-            </div>
-        <nav class="navbar">
-            <div class="main-navbar">
-                ${navbarHTML}
-            </div>
-        </nav>
-        <button class="pink baltas none480px"> Get started</button>
-
+                <div>
+                    <a href = "#"><img class="logo" src="./img/SofaxLogo.webp" alt="logo" ></a>
+                </div>
+                <nav class="navbar">
+                    <div class="main-navbar">
+                   ${navbarHTML}
+                    </div>
+                </nav>
+                <button class="pink baltas none480px"> Get started</button>
             </header>
         </div>
     </div>`;
@@ -29,4 +45,5 @@ export function header() {
     document
         .getElementById('app')
         .insertAdjacentHTML('beforeend', HTML);
+
 }
