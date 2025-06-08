613fbed6bb7f9dc8114f02794a97ad7bde258e71
Ernest <zyperis14@gmail.com>
2025-06-08 20:31:30 +0300

    main navbaras WIP

--- js/components/header.js
+++ js/components/header.js
@@ -11,14 +11,14 @@ export function header() {
 
             for (const subItem of item.subMenu) {
                 subMenuHTML += `
-                <a href="${subItem.href}">${subItem.text}</a>
-            `;
+                <li><a class="dropdown-link" href="${subItem.href}">${subItem.text}</a></li>
+            `
             };
 
             navbarHTML += `
             <div class="dropdown">
                 <a href="${item.href}">${item.text}<i class="fa fa-angle-down"></i></a>
-                <div class="dropdown-list"><i class="fa fa-caret-up top-angle"></i>${subMenuHTML}</div>
+                <div class="dropdown-list"><i class="top-angle fa fa-caret-up"></i><ul>${subMenuHTML}</ul></div>
             </div>`;
         } else {
             navbarHTML += `<div class="dropdown"><a href="${item.href}">${item.text}</a></div>`;
