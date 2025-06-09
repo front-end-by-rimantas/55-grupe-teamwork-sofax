e6e4a01abba20c8a5e9f742ed5d6934345b1953d
Povilas Dailyda <povilas.dailyda@gmail.com>
2025-06-09 15:04:58 +0300

    up

---
+++ js/components/contact-us/contact-us-main.js
@@ -0,0 +1,29 @@
+/*   import {cuMaindata } from "../../data/contact-us-maindata.js";*/
+ import { cuMaindata } from "../../data/contact-us-maindata.js"
+
+export function contactUsMain() {
+    let HTML1 = '';
+    
+    for (const item of cuMaindata) {
+        HTML1 += `
+        <div class="contact-item">
+            <img src="${item.imgInf}" alt="${item.altInf}">
+            <h3>${item.title}</h3> <!-- Pataisyta: buvo item.title.Inf -->
+            <p>${item.desc}</p>
+        </div>`;
+    }
+    
+    const HTML = `
+    <div class="container">
+        <div class="row">
+            <div class="cuKairys">
+                ${HTML1}
+            </div>
+            <div class="cuDesinys"> <!-- Pataisyta: trūko uždarančios kabutės -->
+                <!-- Čia galite įdėti dešinės pusės turinį -->
+            </div>
+        </div>
+    </div>`;
+
+    document.getElementById('app').insertAdjacentHTML('beforeend',HTML)
+}
