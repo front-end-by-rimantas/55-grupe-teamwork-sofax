709060baecd5f0fb65557cfe6611570314d22cfb
Povilas Dailyda <povilas.dailyda@gmail.com>
2025-06-09 14:22:12 +0300

    contact-us-map

---
+++ js/components/contact-us/contact-us-map.js
@@ -0,0 +1,11 @@
+export function CUmap() {
+    const HTML = `
+    <div class="forMap">
+        
+        <img src="../img/map.webp" class="map">
+        
+    </div>
+    `
+    document.getElementById('app').insertAdjacentHTML('beforeend', HTML)
+
+}
