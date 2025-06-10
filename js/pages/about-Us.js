59cf229503359c44ac99e70b5cd336c23f4cacad
Vitalij <203612403+helltah@users.noreply.github.com>
2025-06-10 21:51:00 +0300

    abt-us-main-row-1

--- js/pages/about-Us.js
+++ js/pages/about-Us.js
@@ -1,5 +1,6 @@
 import { aboutUsHeader } from "../components/about-us/about-us-header.js";
+import { aboutUsMainRow1 } from "../components/about-us/about-us-mainRow1.js";
 
 aboutUsHeader();
-
+aboutUsMainRow1();
 
